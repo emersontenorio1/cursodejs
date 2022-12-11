389ea22171a22f5ddb453707ad7f9e72fcdf53ab
Emerson Tenorio <emersontenorio@FB1405>
2022-12-10 21:53:16 -0300

    adicionando infor

---
+++ Fundamentos/controle/if2.js
@@ -0,0 +1,11 @@
+function test1(nun){
+    if(nun > 7)
+        console.log(nun)
+        console.log('final')
+    
+
+}
+
+
+test1(6)
+test1(8)
---
+++ Fundamentos/controle/ifElse.js
@@ -0,0 +1,19 @@
+const imprimirResultado = function(nota){
+   // console.log(typeof(nota))
+
+   if(typeof(nota) == 'number'){ 
+    if(nota>=7){
+        console.log('Aprovado')
+    }else{
+        console.log('Reprovado')
+    }
+}else{
+    console.log("Valor invalido ")
+}
+
+
+}
+
+imprimirResultado(10)
+//imprimirResultado("10")
+imprimirResultado(10)
