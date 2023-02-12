1653d4b1303b86cf70a2488098b69f7e20bb8c4b
Emerson Tenorio <emersontenorio@FB1405>
2023-02-12 12:49:42 -0300

    esses nao foram

--- Fundamentos/helloworld.js
+++ Fundamentos/helloworld.js
@@ -8,10 +8,13 @@ console.log(typeof '3')
 
 alert("emerson0");
 
-
-
-
-
+function cont(){
+    var conteudo = document.getElementById('print').innerHTML;
+    tela_impressao = window.open('about:blank');
+    tela_impressao.document.write(quantidade);
+    tela_impressao.window.print();
+    tela_impressao.window.close();
+ }
 
 
 
